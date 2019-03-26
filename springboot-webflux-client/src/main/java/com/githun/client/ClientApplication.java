437c89e978cd0cfdac38d44ebbe7596cdb7ad677
石少东 <shishaodong@cmhi.chinamobile.com>
2019-03-26 22:48:43 +0800

    更新客户端

---
+++ springboot-webflux-client/src/main/java/com/githun/client/ClientApplication.java
@@ -0,0 +1,17 @@
+package com.githun.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shao
+ */
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
+
